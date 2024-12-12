d7cb0351ae66331ec79f546ea3a5a20174f3a553
4pion13 <dend1ypsn@gmail.com>
2024-12-12 16:49:11 +0300

    Изменен подход позиционирования элементов

--- start.js
+++ start.js
@@ -5,13 +5,9 @@ const closeBtn = document.getElementById('close-work-flow');
 const startText = document.getElementById('start-text');
 console.log(testBlock.innerHTML)
 addButton.onclick = function() {
-    testBlock.style.display = "block";
-    startText.style.display = "none";
     workFlow.classList.add("test-block-resized");
 }
 
 closeBtn.onclick = function() {
-    testBlock.style.display = "none";
-    startText.style.display = "block";
     workFlow.classList.remove("test-block-resized");
 }
