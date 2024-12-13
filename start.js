9cfb035848c819b7ee46a5e3e1481a94c0d4aa16
4pion13 <dend1ypsn@gmail.com>
2024-12-13 04:44:21 +0300

    Добавлено меню добавления заметки

--- start.js
+++ start.js
@@ -3,6 +3,10 @@ const workFlow = document.getElementById('work-flow');
 const testBlock = document.getElementById('work-flow');
 const closeBtn = document.getElementById('close-work-flow');
 const startText = document.getElementById('start-text');
+const noteBtn = document.getElementById('note-btn');
+const noteBlock = document.getElementById('note-block');
+const closeNote = document.getElementById('close-note');
+console.log(noteBlock.innerHTML);
 console.log(testBlock.innerHTML)
 addButton.onclick = function() {
     workFlow.classList.add("test-block-resized");
@@ -10,4 +14,13 @@ addButton.onclick = function() {
 
 closeBtn.onclick = function() {
     workFlow.classList.remove("test-block-resized");
+}
+
+
+noteBtn.onclick = function() {
+    noteBlock.classList.add("note-block-resized");
+}
+
+closeNote.onclick = function() {
+    noteBlock.classList.remove("note-block-resized");
 }
